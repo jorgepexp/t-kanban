8a67ddb785ecb8821e417666b5a1daad50ca0e8c
Jorge P <jorge.pexp@gmail.com>
2023-09-21 17:54:58 +0200

    Fixed wrong install on main repo

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -19,20 +19,11 @@ const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
 const Api = {
   fetchBoard: async (id: string): Promise<Board> => {
-    await sleep(100)
-    return {
-      id: '1',
-      name: 'Tablerico 👍🏻',
-      columns: [
-        { id: '1', name: 'To do', tasks: [] },
-        { id: '2', name: 'Doing', tasks: [] },
-        { id: '3', name: 'Done', tasks: [] },
-      ]
-    }
+    const board = await fetch(`http://localhost:3001/board/${id}`)
+    return board.json();
   },
   async saveBoard(board: Board) {
     await sleep(100)
-    
     console.log('// TODO update en base de datos', board)
   }
 }
