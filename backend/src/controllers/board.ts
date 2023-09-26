9d3fb404253b8cb8b2eb72ba2c5671712fa7f194
Jorge P <jorge.pexp@gmail.com>
2023-09-26 02:35:57 +0200

    Change board controller to return db values Create board endpoint

--- backend/src/controllers/board.ts
+++ backend/src/controllers/board.ts
@@ -1,53 +1,106 @@
 import { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
 
-// TODO Encapsulate
+const prisma = new PrismaClient();
+
 interface Task {
   id: string;
   name: string;
 }
 
-interface Column {
+interface State {
   id: string;
   name: string;
   tasks: Task[] | [];
 }
 
-interface Board {
-  id: string;
-  name: string;
-  columns: Column[] | [];
-}
-
 const BoardController = {
-  getBoard(req: Request, res: Response) {
-    if (!req.params.id) return;
+  async createBoard(req: Request, res: Response) {
+    const board = await prisma.project.create({
+      data: {
+        name: req.body.name,
+      },
+    });
 
-    const data: Board = {
-      id: req.params.id,
-      name: 'Tablerico 👍🏻',
-      columns: [
-        { id: '1', name: 'To do', tasks: [] },
-        { id: '2', name: 'Doing', tasks: [] },
-        { id: '3', name: 'Done', tasks: [] },
-      ],
-    };
-
-    res.send(data);
+    res.status(200).send(board);
   },
-  editBoard(req: Request, res: Response) {
+  async getBoard(req: Request, res: Response) {
     if (!req.params.id) return;
+    const id = Number(req.params.id);
 
-    const data: Board = {
-      id: req.params.id,
-      name: 'Tablerico 👍🏻',
-      columns: [
-        { id: '1', name: 'To do', tasks: [] },
-        { id: '2', name: 'Doing', tasks: [] },
-        { id: '3', name: 'Done', tasks: [] },
-      ],
-    };
+    const project = await prisma.project.findFirst({
+      where: { id },
+    });
+    if (!project) return;
 
-    res.send(data);
+    const states = await prisma.state.findMany({
+      where: { projectId: id },
+      include: {
+        tasks: true,
+      },
+    });
+
+    // TODO No ignorar
+    // @ts-ignore
+    project.states = states;
+
+    res.status(200).send(project);
+  },
+  async editBoard(req: Request, res: Response) {
+    if (!req.params.id) return;
+    const id = Number(req.params.id);
+
+    await prisma.project.update({
+      where: {
+        id,
+      },
+      data: {
+        name: req.body.name,
+      },
+    });
+
+    const statesWithoutTasks = req.body.states.map(
+      ({ tasks, ...rest }: { tasks: Task[] }) => rest
+    );
+
+    for (const state of statesWithoutTasks) {
+      if (state.id === -1) {
+        await prisma.state.create({
+          data: {
+            name: state.name,
+            projectId: state.projectId,
+          },
+        });
+        break;
+      }
+      await prisma.state.update({
+        where: {
+          id: state.id,
+        },
+        data: state,
+      });
+    }
+
+    const tasks = req.body.states.flatMap((state: State) => state.tasks);
+    for (const task of tasks) {
+      if (task.id === -1) {
+        await prisma.task.create({
+          data: {
+            name: task.name,
+            stateId: task.stateId,
+            projectId: task.projectId,
+          },
+        });
+      } else {
+        await prisma.task.update({
+          where: {
+            id: task.id,
+          },
+          data: task,
+        });
+      }
+    }
+    res.status(200).send(req.body);
   },
 };
 
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,27 +1,19 @@
 import express from 'express';
 import cors from 'cors';
-
-import { PrismaClient } from '@prisma/client';
+import BoardController from './controllers/board';
 
 const app = express();
 const port = 3001;
 
-const prisma = new PrismaClient();
-
 app.use(express.json());
 app.use(cors({ origin: 'http://localhost:3000' }));
 
-app.get('/api/boards/:id', async (req, res) => {
-  const id = Number(req.params.id);
+app.get('/api/boards/:id', BoardController.getBoard);
 
-  const project = await prisma.project.findFirst({
-    where: { id },
-  });
+app.put('/api/boards/:id', BoardController.editBoard)
 
-  res.send(project);
-});
+app.post('/api/boards', BoardController.createBoard);
 
-// TODO: Hacer el put
 
 app.listen(port, () => {
   console.log('🔥 Server up and running on http://localhost:%d', port);
